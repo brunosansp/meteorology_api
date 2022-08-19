c1926f1e68c5a02228cf90e355593fbbb1427c2b
shogun_dev <brunoadsjp@gmail.com>
2022-08-18 23:54:56 -0300

    start project and set configs step1

---
+++ run.sh
@@ -0,0 +1,14 @@
+# create project
+$ npm init
+
+# add typescript
+$ yarn add -D typescript
+
+# add types node em ambiente de DEV
+$ yarn add -D @types/node
+
+# add module-alias para fazer os paths que estarão no tsconfig.json funcionarem
+$ yarn add module-alias
+  ## add types do module-alias em ambiente de DEV
+  $ yarn add -D @types/module-alias
+
